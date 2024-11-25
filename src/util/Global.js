1c488d6668f17e8c08483ad315e187e7ca353b19
hreinoso <hreinosoflores@gmail.com>
2024-11-25 18:44:37 -0500

    librerias actualizadas 2024, formateado con eslint

--- src/util/Global.js
+++ src/util/Global.js
@@ -1,7 +1,6 @@
 //Variables globales para ser utilizadas en todo el proyecto
 export var Global = {
-    //url : 'http://localhost:40009/api/',
-    url: 'http://192.168.1.3:45455/api/',
+    url : 'http://localhost:40009/api/',
     urlCursos : 'Cursos/',
     urlBandeja : 'BandejaMensajes/',
     title : 'UNALM | '
